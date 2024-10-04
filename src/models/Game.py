dafcbfde682058b84fd81a92dcef837776a7615a
uptc20202 <109162971+uptc20202@users.noreply.github.com>
2024-10-03 22:56:39 -0500

    Avances Simulación

--- src/models/Game.py
+++ src/models/Game.py
@@ -8,6 +8,13 @@ class Game:
         teamA = self.generate_teams("Cara")
         teamB = self.generate_teams("Sello")
         self.simulation = Simulation(teamA,teamB)
+        self.player_max__luchy = []
+        self.player_max_experience = []
+        self.champions_teams_with_scores = []
+        self.champions_woman_amount = 0
+        self.champions_man_amount = 0
+        self.game_count = 0
+
 
     def generate_teams(self, id_team):
         players = []
