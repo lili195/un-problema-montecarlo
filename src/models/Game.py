bff2dcce3d5dd1d47ba90f727d5497dd040b3c9c
uptc20202 <109162971+uptc20202@users.noreply.github.com>
2024-10-04 23:52:23 -0500

    Avances Simulación

--- src/models/Game.py
+++ src/models/Game.py
@@ -8,12 +8,15 @@ class Game:
         teamA = self.generate_teams("Cara")
         teamB = self.generate_teams("Sello")
         self.simulation = Simulation(teamA,teamB)
-        self.player_max__luchy = []
-        self.player_max_experience = []
+        self.max__luchy = []
         self.champions_teams_with_scores = []
+        self.players_scores = []
+        self.max_experience = []
         self.champions_woman_amount = 0
         self.champions_man_amount = 0
         self.game_count = 0
+        self.count_men = 0
+        self.count_women = 0
 
 
     def generate_teams(self, id_team):
@@ -27,3 +30,43 @@ class Game:
     def start(self):
         for i in range(10):
             self.simulation.start_rount()
+
+    
+
+    def check_gender_champion(self):
+        if self.simulation.champion != None:
+            for player in self.simulation.champion.players:
+                if player.gender == "Male":
+                    self.count_men+=1
+                else: 
+                    self.count_women+=1  
+    
+    def update_experience_list(self): 
+         players = self.teamA.players + self.teamB.players
+         max_experience_player  = max(players, key=lambda x: x.experience)
+         self.max_experience.append(max_experience_player.name)
+
+    def check_players_scores(self): 
+         players = self.teamA.players + self.teamB.players
+         count = 0
+         for player in players:
+            self.players_scores[count].append(player.score)
+            count+=1
+
+    def check_winner(self): 
+        if self.simulation.champion != None:
+            self.champions_teams_with_scores.append(self.simulation.champion)
+        else:
+             self.champions_teams_with_scores.append("Empate")
+    
+    def reset_game(self):
+        self.game_count+=1
+        teamA = self.generate_teams("Cara")
+        teamB = self.generate_teams("Sello")
+        self.simulation = Simulation(teamA,teamB) 
+
+    def update_lucky_list(self): 
+        players = self.teamA.players + self.teamB.players
+        max_lucky_player  = max(players, key=lambda x: x.luck)
+        self.max_lucky.append(max_lucky_player.name)
+        
--- src/models/Simulation.py
+++ src/models/Simulation.py
@@ -19,4 +19,26 @@ class Simulation:
             else:
                 self.champion = None 
             players = self.teamA.players + self.teamB.players 
-            max(players, key=lambda x: x.rounds_won)         
+            max(players, key=lambda x: x.rounds_won)
+
+        else:
+            self.init_players    
+
+    def init_players(self):
+        for player in self.teamA.players + self.teamB.players:
+            player.luck = player.generate_luck()
+            player.round_score = 0
+            player.add_wear()
+            player.current_resistance = player.resistance
+                 
+
+    def simulate_shots(self, team):
+        for player in team.players:
+            while player.can_continue_shooting():
+                score = player.toShot()
+                player.add_roundScore(score)
+                team.update_score(score)
+
+
+
+              
