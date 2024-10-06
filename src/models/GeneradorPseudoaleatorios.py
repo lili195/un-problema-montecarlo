1ecdb32f73e8cd11ffae89178f0c43465b7868e8
lili_ana <lilianah33446@gmail.com>
2024-10-06 18:38:38 -0500

    limpieza de funciones que no se estan usando

--- src/models/GeneradorPseudoaleatorios.py
+++ src/models/GeneradorPseudoaleatorios.py
@@ -1,44 +1,11 @@
 import csv
 import random
-import math
-# import datetime
 import numpy as np
-
-# # Parámetros del LCG 
-# a = 1664525                                         # multiplicador
-# c = 111231313                                       # incremento
-# m = 2**32                                           # módulo
-# X0 = datetime.datetime.now().microsecond            # semilla inicial
 
 #Listas de numeros
 lcg_randomNums = []
 normal_dist_nums = []
 uniform_dist_nums= []
-
-# def generate_numbers(n):
-#     multiplier = is_valid_module(a,m)
-#     return lcg(X0, multiplier, c, m, n)
-
-
-def is_valid_module(a, m):
-    # Verificar que a y m sean primos relativos (gcd debe ser 1)
-    if math.gcd(a, m) != 1:
-        return is_valid_module(a+1,m)
-    # Verificar que a no sea múltiplo de 2
-    if a % 2 == 0:
-        return is_valid_module(a+1,m)
-    #"Los parámetros son válidos."
-    return a
-
-# Función LCG para generar números pseudoaleatorios
-def lcg(seed, a, c, m, n):
-    numbers = []
-    Xn = seed
-    for _ in range(n):
-        Xn = (a * Xn + c) % m
-        numbers.append(Xn / m)  # Normalizar los números entre 0 y 1
-    return numbers
-
     
 # Leer el archivo CSV y extraer los números
 def read_csv(csvFile):
@@ -51,14 +18,12 @@ def read_csv(csvFile):
 # Leer los números desde el archivo CSV
 lcg_randomNums = read_csv("pseudo_random_sequence.csv")
 
-# Mezclar los números aleatoriamente
-
-
-# Función para obtener el próximo número sin reemplazo
+# Función para obtener numeros generados con el metodo de LCG
 def get_nums_zero_one():
     if not lcg_randomNums:
         lcg_randomNums.extend(read_csv("pseudo_random_sequence.csv"))
 
+    # Mezclar los números aleatoriamente
     random.shuffle(lcg_randomNums)
     return lcg_randomNums.pop(0)
  
